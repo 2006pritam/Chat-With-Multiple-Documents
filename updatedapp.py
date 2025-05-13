65b5ffcb08605cada2e17a45d3fd4cf7079eb085
Faisal Hossain <faisal.ece18@gmail.com>
2025-05-13 06:30:14 +0600

    Memory Added

--- updatedapp.py
+++ updatedapp.py
@@ -1,17 +1,12 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import requests
 import streamlit as st
-from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from app import user_input
 from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain_google_genai import ChatGoogleGenerativeAI
